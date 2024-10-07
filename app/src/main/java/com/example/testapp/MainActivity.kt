5767fc2f6d04866d380cac253ffdfc8d963ca2cd
Каюн Максим <maks.kayun@yandex.ru>
2024-10-07 18:38:45 +0300

    styles impl

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -3,84 +3,24 @@ package com.example.testapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.testapp.ui.theme.TestAppTheme
 import com.example.testapp.main.MainScreen
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            TestAppTheme() {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(start = 16.dp, end = 16.dp),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
                     MainScreen()
                 }
             }
         }
-    }
-
-    @Composable
-    fun RowGreeting(modifier: Modifier = Modifier) {
-        Row(modifier) {
-            Greeting("Android")
-            Greeting("Android")
-        }
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestAppTheme() {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/testapp/main/MainScreen.kt
+++ app/src/main/java/com/example/testapp/main/MainScreen.kt
@@ -1,66 +1,143 @@
 package com.example.testapp.main
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.domain.entity.ListElement
+import com.example.testapp.R
 import com.example.testapp.main.vm.MainState
 import com.example.testapp.main.vm.MainViewModel
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel = koinViewModel()) {
     val state by viewModel.state.collectAsState()
-    Box(modifier = Modifier.fillMaxSize()) {
-        when (val st = state) {
-            is MainState.Content -> {
-                ContentState(list = st.list)
-            }
-
-            is MainState.Error -> {
-                ErrorState(st.message)
-            }
-
-            MainState.Loading -> {
-                Text(text = "Загрузка...")
-            }
-        }
-    }
-}
-
-@Composable
-fun ErrorState(message: String) {
-    Text(modifier = Modifier.fillMaxWidth(), text = message)
-}
-
-@Composable
-fun ContentState(list: List<ListElement>) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        list.forEach { element ->
-            Row(modifier = Modifier.padding(vertical = 8.dp)) {
-                AsyncImage(
-                    modifier = Modifier.size(136.dp),
-                    model = element.image,
-                    contentScale = ContentScale.Crop,
-                    contentDescription = null
+    Column {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Title",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(start = 32.dp)
                 )
-                Spacer(modifier = Modifier.width(24.dp))
-                Text(text = element.title)
+            },
+            actions = {
+                Icon(
+                    painter = painterResource(id = R.drawable.icon),
+                    contentDescription = null,
+                    modifier = Modifier.padding(end = 18.dp)
+                )
+            })
+        Box(modifier = Modifier.fillMaxWidth()) {
+            when (val st = state) {
+                is MainState.Content -> {
+                    ContentState(list = st.list)
+                }
+
+                is MainState.Error -> {
+                    Text(text = "Error")
+                }
+
+                MainState.Loading -> {
+                    LoadingState()
+                }
             }
         }
     }
+}
+
+@Composable
+private fun LoadingState() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .align(Alignment.Center)
+        )
+    }
+}
+
+@Composable
+private fun ContentState(list: List<ListElement>) {
+    LazyColumn {
+        item {
+            list.forEach { element ->
+                ElementRow(element)
+            }
+        }
+    }
+}
+
+@Composable
+private fun ElementRow(element: ListElement) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(136.dp)
+                .clip(shape = RoundedCornerShape(28.dp))
+                .shadow(8.dp, shape = RoundedCornerShape(28.dp))
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            MaterialTheme.colorScheme.primary,
+                            MaterialTheme.colorScheme.secondary
+                        )
+                    )
+                )
+        ) {
+            AsyncImage(
+                model = element.image,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(shape = RoundedCornerShape(28.dp)),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Column(modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)) {
+            Text(text = element.title, style = MaterialTheme.typography.headlineSmall)
+            if (element.subtitle != null) {
+                Text(
+                    text = element.subtitle.toString(),
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                element.button?.title?.let {
+                    Text(
+                        text = it,
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
